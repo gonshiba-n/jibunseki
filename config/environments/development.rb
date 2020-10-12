2301e3232f07c5ef3d000fa6bc1f09f2a0d81500
gonshiban <nss.0120g@gmail.com>
2020-10-13 00:31:18 +0900

    bCryptのストレッチイング回数指定(約4000回ハッシュ化を行う)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  require 'bcrypt'
+  # ストレッチ回数
+  BCrypt::Engine::DEFAULT_COST = 12
 end
