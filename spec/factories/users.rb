87103ca62797dec692c410b929a11ce3f8975118
gonshiban <nss.0120g@gmail.com>
2020-10-14 01:05:00 +0900

    requestsテストを作成。static_pagesとusersは完了

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
 	factory :user do
+		id {1}
 		name { 'test_user' }
 		email { 'test@example.com' }
 		password { 'password' }
+
+		trait :invalid do
+			password {'pass'}
+    end
 	end
 end
