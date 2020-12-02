01a01c0306977eac236e265b54b992d2487783cb
gonshiban <nss.0120g@gmail.com>
2020-12-02 23:36:11 +0900

    has_oneメソッドによるアソシエーション変更

--- app/controllers/guidelines_controller.rb
+++ app/controllers/guidelines_controller.rb
@@ -1,16 +1,16 @@
 class GuidelinesController < ApplicationController
   def create
     respond_to do |format|
-      case Guideline.exists?(user_id: @current_user.id)
-      when false
-        @guideline = Guideline.new(guideline_params)
+      case @current_user.guideline
+      when nil
+        @guideline = @current_user.build_guideline(guideline_params)
         if @guideline.save(context: :user)
           format.js { flash.now[:success] = "行動指針を作成しました。" }
         else
           format.js { render :create_errors }
         end
-      when true
-        @guideline = Guideline.find_by(user_id: @current_user.id)
+      when @current_user.guideline
+        @guideline = @current_user.guideline
         if @guideline.update(guideline_params)
           format.js { flash.now[:success] = "行動指針をアップデートしました。" }
         else
@@ -25,6 +25,6 @@ class GuidelinesController < ApplicationController
   private
 
   def guideline_params
-    params.require(:guideline).permit(:text).merge(user_id: @current_user.id)
+    params.require(:guideline).permit(:text)
   end
 end
