35c62a9e7f70c82081b3836633cd700e94de81b3
gonshiban <nss.0120g@gmail.com>
2020-12-07 22:48:42 +0900

    ゲストログイン追加。seedデータの追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,10 @@ class SessionsController < ApplicationController
 
   def new
     @user = User.new
+    if params[:user_id]
+      @guest_user = User.find(params[:user_id])
+      @pass = "password"
+    end
   end
 
   # ログイン処理
---
+++ db/fixtures/01_users.rb
@@ -0,0 +1,6 @@
+User.seed(:id) do |s|
+  s.id = 1
+  s.name = "ゲストユーザー"
+  s.email = "gestuser@jibunseki.com"
+  s.password = "password"
+end
