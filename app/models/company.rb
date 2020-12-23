a1dd179fa941a0861ab150a21310121529348e76
gonshiban <nss.0120g@gmail.com>
2020-12-24 02:47:15 +0900

    compnayモデルのvalidation追加

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,27 @@
 class Company < ApplicationRecord
   belongs_to :user
 
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :url, presence: true
+  validates :url, format: /\A#{URI::regexp(%w(http https))}\z/
+
+  with_options length: { maximum: 800 } do
+    validates :business,
+              :culture,
+              :vision,
+              :future,
+              :skill,
+              :treatment
+  end
+  with_options numericality: {only_integer: true, greater_than: 0, less_than: 6} do
+    validates :business_fit,
+              :culture_fit,
+              :vision_fit,
+              :future_fit,
+              :skill_fit,
+              :treatment_fit
+  end
+
   def aspiration
     result = [
             business_fit,
@@ -12,8 +33,4 @@ class Company < ApplicationRecord
           ]
     result.sum
   end
-
-  place_holder = {
-    business: "事業内容の共感するポイントをまとめましょう。"
-  }
 end
