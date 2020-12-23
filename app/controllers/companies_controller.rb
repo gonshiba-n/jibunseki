e42d98686516ef54d8e32c8a1eaf35d31ad26afd
gonshiban <nss.0120g@gmail.com>
2020-12-24 02:48:14 +0900

    companyセクション新規作成、削除機能追加。新規作成のみAjax化

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,16 +1,34 @@
 class CompaniesController < ApplicationController
   def create
-    @campany = @current_user.company.new(targets_params)
-    if @campany.save
-      redirect_to user_path(@current_user.id), notice: "#{@company.name}の登録が完了しました。"
+    @company = @current_user.company.new(companies_params)
+    respond_to do |format|
+      if @company.save
+        format.js { flash.now[:success] = "#{@company.name}を登録しました。"  }
+      else
+        redirect_to user_path(@current_user.id), notice: "登録ができませんでした。"
+      end
+      set_instance
+    end
+  end
+
+  def destroy
+    if select_companies_params.present?
+      select_companies_params.each do |company|
+        @company = @current_user.company.find(company)
+        if @company.delete
+          redirect_to user_path(@current_user.id), notice: "削除しました。"
+        else
+          redirect_to user_path(@current_user.id), notice: "削除できませんでした。"
+        end
+      end
     else
-      redirect_to user_path(@current_user.id), notice: "登録ができませんでした。"
+      redirect_to user_path(@current_user.id), notice: "削除項目を選択してください。"
     end
   end
 
   private
 
-  def targets_params
+  def companies_params
     params.require(:company).permit(:id,
                                     :name,
                                     :url,
@@ -27,5 +45,14 @@ class CompaniesController < ApplicationController
                                     :treatment,
                                     :treatment_fit,
                                   )
+  end
+
+  def select_companies_params
+    ids = params.fetch(:company, {}).permit(companies_ids: [])
+    ids.values[0]
+  end
+
+  def set_instance
+    @companies = Company.where(user_id: @current_user.id)
   end
 end
